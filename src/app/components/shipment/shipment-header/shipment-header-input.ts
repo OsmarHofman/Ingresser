246832a5b2dd0e12b497a8c722bf181b50f1c2f4
Osmar José Hofman da Silva <osmar.silva@ndd.tech>
2024-08-28 00:22:33 -0300

    feat: Alterado estilo dos campos do header e adicionados ao form

--- src/app/components/shipment/shipment-header/shipment-header-input.ts
+++ src/app/components/shipment/shipment-header/shipment-header-input.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { GridComponent } from '../../grid/grid';
 import { CarrierInputContainerComponent } from "./carrier-input-container/carrier-input-container";
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'shipment-header-input',
@@ -11,7 +13,10 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
     imports: [
         GridComponent,
         CarrierInputContainerComponent,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatInputModule,
+        MatFormField,
+        MatLabel,
     ],
 })
 
@@ -23,6 +28,10 @@ export class ShipmentHeaderInputComponent {
     ngOnInit(): void {
         this.shipmentHeaderForm = this.formBuilder.group({
             shipmentDomainName: [''],
+            shipmentXid: [''],
+            travelStatus: [''],
+            emissionStatus: [''],
+            shipmentTaker: [''],
             shipmentCarrier: ['']
         });
     }
