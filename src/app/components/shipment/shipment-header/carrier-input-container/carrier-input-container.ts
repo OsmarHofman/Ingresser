0be50c81e2b6be7482b074fd79819a0c9f521598
Osmar José Hofman da Silva <osmar.silva@ndd.tech>
2024-08-27 22:19:32 -0300

    feat: Criado estrutura form para transportador do shipmentHeader

--- src/app/components/shipment/shipment-header/carrier-input-container/carrier-input-container.ts
+++ src/app/components/shipment/shipment-header/carrier-input-container/carrier-input-container.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'carrier-input-container',
@@ -8,9 +9,58 @@ import { MatCardModule } from '@angular/material/card';
     styleUrl: 'carrier-input-container.scss',
     standalone: true,
     imports: [
-        MatCardModule
+        MatCardModule,
+        ReactiveFormsModule
     ],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: CarrierInputContainerComponent
+        }
+    ]
 })
 
-export class CarrierInputContainerComponent {
+export class CarrierInputContainerComponent implements ControlValueAccessor, OnDestroy {
+
+    carrierInputForm: FormGroup = this.formBuilder.group({
+        domainName: [''],
+        xid: ['']
+    });
+
+    onTouched: Function = () => { };
+
+    onChangeSubs: Subscription[] = [];
+
+
+    constructor(private formBuilder: FormBuilder) { }
+
+
+    ngOnDestroy(): void {
+        this.onChangeSubs.forEach(sub => {
+            sub.unsubscribe();
+        });
+    }
+
+    registerOnChange(onChange: any): void {
+        const sub = this.carrierInputForm.valueChanges.subscribe(onChange);
+        this.onChangeSubs.push(sub);
+    }
+
+    registerOnTouched(onTouched: any): void {
+        this.onTouched = onTouched;
+    }
+
+    setDisabledState?(isDisabled: boolean): void {
+        if (isDisabled)
+            this.carrierInputForm.disable();
+        else
+            this.carrierInputForm.enable();
+    }
+
+    writeValue(value: any): void {
+        if (value)
+            this.carrierInputForm.setValue(value, { emitEvent: false });
+    }
+
 }
--- src/app/components/shipment/shipment-header/shipment-header-input.ts
+++ src/app/components/shipment/shipment-header/shipment-header-input.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { GridComponent } from '../../grid/grid';
 import { CarrierInputContainerComponent } from "./carrier-input-container/carrier-input-container";
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'shipment-header-input',
@@ -10,8 +11,23 @@ import { CarrierInputContainerComponent } from "./carrier-input-container/carrie
     imports: [
         GridComponent,
         CarrierInputContainerComponent,
+        ReactiveFormsModule
     ],
 })
 
 export class ShipmentHeaderInputComponent {
+    shipmentHeaderForm: any;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.shipmentHeaderForm = this.formBuilder.group({
+            shipmentDomainName: [''],
+            shipmentCarrier: ['']
+        });
+    }
+
+    submitForm(): void {
+        console.log('Formulario:', this.shipmentHeaderForm.value);
+    }
 }
